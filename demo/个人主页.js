1dfb75f70ddce01e911b593b1d60b8f4a95b91e3
zxc0406 <38043274+zxc0406@users.noreply.github.com>
2018-04-10 09:38:37 +0800

    Add files via upload

--- demo/个人主页.js
+++ demo/个人主页.js
@@ -29,9 +29,31 @@ function showPic(whichpic){
 
 var input=document.getElementsByTagName("textarea");
 var orign=input[0].firstChild.nodeValue;
-input[0].onclick=function(){
+input[0].onfocus=function(){
     input[0].firstChild.nodeValue="";
 }
-input[0].onmouseout=function(){
-    input[0].firstChild.nodeValue=orign;
+input[0].onblur=function(){
+    if(input[0].firstChild.nodeValue==""){
+        input[0].firstChild.nodeValue=orign;
+    }
 }
+
+// var wordgallery=document.getElementsByClassName("friends");
+// for(var i=0;i<wordgallery.length;i++){
+//     var link[i]=gallery.getElementsByTagName("div");
+//     function showword(whichword){
+//     var b=whichword.getAttribute("title");
+//     var transword=getElementsByClassName("transword");
+//     transword.
+//     }
+// }
+
+// var a=new Date();
+// var year=a.getFullYear();
+// var b=document.getElementsByClassName("foot");
+// var time=b.getElementsByTagName("ul").getElementsByTagName("li");
+// time[0].firstChild.nodeValue=year;
+
+
+
+
